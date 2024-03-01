4aea11d6ba4d9646a744e4dcd102b2df3bbf7086
Alex <kostikovaleksej20@gmail.com>
2024-03-01 19:25:17 +0300

    my changes

---
+++ src/script/LogReg.js
@@ -0,0 +1,15 @@
+// Получаем ссылку на кнопку "Sign up"
+var signUpButton = document.getElementById("SignUuBTN");
+
+
+signUpButton.onclick = function() {
+    if(document.getElementById("firstRadio").checked == true){
+        window.location.href = "../../public/userCont/aplicant.html"
+    }
+    if(document.getElementById("secondRadio").checked == true){
+        window.location.href = "../../public/userCont/recruter.html"
+    }
+    else{
+        console.log("error")
+    }
+}
